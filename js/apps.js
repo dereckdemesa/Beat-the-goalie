66865743f228fcab0013b4ae859907b92ea795b7
dereck1988@gmail.com <dereckdemesa@Derecks-MacBook-Pro.local>
2024-04-15 22:45:49 -0700

    feat: index.html

--- js/apps.js
+++ js/apps.js
@@ -6,9 +6,9 @@ let counter = 0;
 let playerScore = 0;
 let aiScore = 0;
 
-// Function for players jump
+// function for when you press the spacebar it jumps for the player
 function jump() {
-  console.log('Player jumps...');
+  // console.log('Player jumps...');
   if (character.classList.contains("animate")) {
     return;
   }
@@ -17,3 +17,24 @@ function jump() {
     character.classList.remove("animate");
   }, 300);
 }
+
+// event listener for keydown event
+document.addEventListener('keydown', jump);
+
+// check crash and the score
+const checkDead = setInterval(() => {
+    // console.log('checking collision and score')
+    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+      block.style.animation = "none";
+      alert("Game Over. Your score: " + playerScore);
+      counter = 0;
+      playerScore = 0;
+      block.style.animation = "block 1s infinite linear";
+    } else {
+      counter++;
+      scoreSpan.textContent = Math.floor(counter / 100);
+      playerScore = Math.floor(counter / 100); // Update player's score
+    }
+  }, 10);
