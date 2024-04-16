c07108b57ea0660c926b1360961a405ff21ec553
dereck1988@gmail.com <dereckdemesa@Derecks-MacBook-Pro.local>
2024-04-15 17:01:12 -0700

    apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,26 +1,19 @@
-const gameArea = document.querySelector('.game-area');
-const scoreDisplay = document.getElementById('score');
-const currentPlayerDisplay = document.getElementById('currentPlayer');
+const character = document.getElementById("character");
+const block = document.getElementById("block");
+const scoreSpan = document.getElementById("scoreSpan");
 
-let scores = [0, 0];
-let currentPlayer = 0;
-let lastClickTime = 0;
+let counter = 0;
+let playerScore = 0;
+let aiScore = 0;
 
-// Creating new a new circle with this function
-function createCircle() {
-    // console.log('Creating circle..');
-    const circle = document.createElement('div');
-    circle.classList.add('circle');
-    circle.addEventListener('click', () => {
-        const now = Date.now();
-        // checks if the player clicks too fast
-        if (now -lastClickTime < 1000) {
-            alert('Too Fast! You Lose!');
-            scores[currentPlayer] = 0;
-        } else {
-            scores [currentPlayer]++;
-        }
-        lastClickTime = now;
-        scoreDisplay.textContent = scores[currentPlayer];
-    }
-)}
+// Function for players jump
+function jump() {
+  console.log('Player jumps...');
+  if (character.classList.contains("animate")) {
+    return;
+  }
+  character.classList.add("animate");
+  setTimeout(() => {
+    character.classList.remove("animate");
+  }, 300);
+}
